99b561126a8ce52ac1d302dac2aa3b8250b43a63
Vignesh Ashokan <vigneshgashokan@gmail.com>
2018-12-22 17:22:20 +0530

    added authentication using google OAuth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -8,9 +10,10 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return(
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
           <Route path="/streams/edit" component={StreamEdit} />
